10c5732c28222ba7865d7687704d2f8d1ec6c16c
Сергей <hohachka@mail.ru>
2022-01-28 15:34:54 +0300

    Update and rename mysite/settings.py to майсайт/settings.py

--- майсайт/settings.py
+++ майсайт/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9bx5$!(2tp^e+ujlf_6i#2-9c9@sx6wf*y3j)l08j*4e_2q4x2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
